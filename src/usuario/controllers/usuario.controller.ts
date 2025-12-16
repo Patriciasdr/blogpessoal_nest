6677f9ccb2c781b96829e10b4178523fcdfff895
PatriciaSouza <patriciasdr@hotmail.com>
2025-12-16 16:50:23 -0300

    Deploy do Projeto Blog Pessoal

--- src/usuario/controllers/usuario.controller.ts
+++ src/usuario/controllers/usuario.controller.ts
@@ -1,11 +1,13 @@
-import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put, UseGuards } from "@nestjs/common";
-import { UsuarioService } from "../services/usuario.service";
-import { Usuario } from "../entities/usuario.entity";
+import { Body, Controller, Get, HttpCode, HttpStatus, Post, Put, UseGuards, Param, ParseIntPipe } from "@nestjs/common";
+import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
 import { JwtAuthGuard } from "../../auth/guard/jwt-auth.guard";
+import { Usuario } from "../entities/usuario.entity";
+import { UsuarioService } from "../services/usuario.service";
 
+@ApiTags('Usuario')
 @Controller("/usuarios")
+@ApiBearerAuth()
 export class UsuarioController {
-
     constructor(private readonly usuarioService: UsuarioService) { }
 
     @UseGuards(JwtAuthGuard)
@@ -14,31 +16,25 @@ export class UsuarioController {
     findAll(): Promise<Usuario[]> {
         return this.usuarioService.findAll();
     }
-
+    
     @UseGuards(JwtAuthGuard)
     @Get('/:id')
     @HttpCode(HttpStatus.OK)
-    findById(@Param('id', ParseIntPipe) id: number): Promise<Usuario> {
+    findById(@Param('id', ParseIntPipe) id: number): Promise<Usuario>{
         return this.usuarioService.findById(id)
     }
 
-    @Post('/cadastrar')
     @HttpCode(HttpStatus.CREATED)
+    @Post('/cadastrar')
     async create(@Body() usuario: Usuario): Promise<Usuario> {
-        return this.usuarioService.create(usuario)
+        return await this.usuarioService.create(usuario);
     }
 
     @UseGuards(JwtAuthGuard)
     @Put('/atualizar')
     @HttpCode(HttpStatus.OK)
     async update(@Body() usuario: Usuario): Promise<Usuario> {
-        return this.usuarioService.update(usuario)
+        return this.usuarioService.update(usuario);
     }
 
-    @UseGuards(JwtAuthGuard)
-    @Delete('/deletar/:id')
-    @HttpCode(HttpStatus.NO_CONTENT)
-    async delete(@Param('id', ParseIntPipe) id: number): Promise<void> {
-        return this.usuarioService.delete(id);
-    }
 }
